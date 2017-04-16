910f06a63cbcdecbeaca4681163942d63ee014aa
Alfred Conlan <oconellan@gmail.com>
2017-04-15 23:43:12 -0500

    Basic HTML complete

--- default.js
+++ default.js
@@ -1,1 +1,5 @@
+//url to perform search
+// https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?"
 
+//url to perform random article retrieval
+//https://en.wikipedia.org/wiki/Special:Random
